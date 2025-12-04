459b86e15f2caae10a03b305f877b144726d2870
Happy-huh <onyedikas019@gmail.com>
2025-12-04 10:53:16 +0100

    updated new functionality

--- worker/src/remotion/Root.jsx
+++ worker/src/remotion/Root.jsx
@@ -8,29 +8,12 @@ export const RemotionRoot = () => {
       <Composition
         id="MainSequence"
         component={VideoComposition}
-        durationInFrames={300}
+        durationInFrames={300} // This is a default. Overridden by inputProps if passed.
         fps={30}
         width={1920}
         height={1080}
         defaultProps={{
-          layers: [
-            {
-              id: 'layer-1',
-              type: 'text',
-              text: 'Welcome to Onera AI',
-              startFrame: 0,
-              durationInFrames: 150,
-              color: 'white'
-            },
-            {
-              id: 'layer-2',
-              type: 'text',
-              text: 'Rendering Engine',
-              startFrame: 150,
-              durationInFrames: 150,
-              color: '#00e5ff'
-            }
-          ]
+          layers: []
         }}
       />
     </>
